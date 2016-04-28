12c522dfb9eb36431af507ac97f4d3ba99fb9d0d
richardatval <richard.s@valonline.org.uk>
2016-04-28 09:30:55 +0100

    Emulate php search more closely

--- _includes/js/lunrfeed.js
+++ _includes/js/lunrfeed.js
@@ -43,48 +43,44 @@ var store = [{% for post in site.posts %}
 // builds search
 $(document).ready(function() {
 	var getUrlParameter = function getUrlParameter(sParam) {
-	    var sPageURL = decodeURIComponent(window.location.search.substring(1)),
-	        sURLVariables = sPageURL.split('&'),
-	        sParameterName,
-	        i;
+		var sPageURL = decodeURIComponent(window.location.search.substring(1)),
+			sURLVariables = sPageURL.split('&'),
+			sParameterName,
+			i;
 
-	    for (i = 0; i < sURLVariables.length; i++) {
-	        sParameterName = sURLVariables[i].split('=');
+		for (i = 0; i < sURLVariables.length; i++) {
+			sParameterName = sURLVariables[i].split('=');
 
-	        if (sParameterName[0] === sParam) {
-	            var returnvalue = sParameterName[1] === undefined ? true : sParameterName[1];
+			if (sParameterName[0] === sParam) {
+				var returnvalue = sParameterName[1] === undefined ? true : sParameterName[1];
 				return returnvalue.split("+").join(" ");
-	        }
-	    }
+			}
+		}
 	};
 	var initialSearch = getUrlParameter('keys');
 	if (initialSearch != undefined) {
-	$('input#search').val(function() {
-	return initialSearch;
-	});
-	}
-  $('input#search').on('keyup', function () {
-    var resultdiv = $('#results');
-    // Get query
-    var query = $(this).val();
-    // Search for it
-    var result = index.search(query);
-    // Show results
-    resultdiv.empty();
-    // Add status
-    resultdiv.prepend('<p class="">Found '+result.length+' result(s)</p>');
-    // Loop through, match, and add results
-	for (var item in result) {
-		var ref = result[item].ref;
-		var searchitem = '<div class="result"><div class="result-body">'+'<a href="'+store[ref].link+'" class="post-title">'+store[ref].title+'</a>';
-		if (typeof store[ref].date != "undefined") {
-			searchitem += '<div class="post-date small">'+store[ref].date+'</div>';
+		$('input#edit-keys').val(function() {
+			return initialSearch;
+		});
+		var resultdiv = $('#results');
+		// Get query
+		var query = initialSearch;
+		// Search for it
+		var result = index.search(query);
+		// Show results
+		resultdiv.empty();
+		// Add status
+		resultdiv.prepend('<p class="">Found ' + result.length + ' result(s)</p>');
+		// Loop through, match, and add results
+		for (var item in result) {
+			var ref = result[item].ref;
+			var searchitem = '<div class="result"><div class="result-body">' + '<a href="' + store[ref].link + '" class="post-title">' + store[ref].title + '</a>';
+			if (typeof store[ref].date != "undefined") {
+				searchitem += '<div class="post-date small">' + store[ref].date + '</div>';
+			}
+			searchitem += '<p class="summary">' + store[ref].summary + '</p>';
+			searchitem += '</div></div>';
+			resultdiv.append(searchitem);
 		}
-		searchitem += '<p class="summary">'+store[ref].summary+'</p>';
-		searchitem +='</div></div>';
-		resultdiv.append(searchitem);
 	}
-});
-
-	$('input#search').triggerHandler('keyup');
 });
