947ed774356e3cad87754171bd91be2ed96e1bcd
richardatval <richard.s@valonline.org.uk>
2016-05-06 12:10:11 +0100

    Add validation to the contact form

--- _includes/js/scripts.js
+++ _includes/js/scripts.js
@@ -1,72 +1,82 @@
 $(document).ready(function() {
 
-	// Menu Bar
+		// Menu Bar
 
-	var menuToggle = $('#js-mobile-menu').unbind();
+		var menuToggle = $('#js-mobile-menu').unbind();
 
-	menuToggle.on('click', function(e) {
-		e.preventDefault();
-		$('#js-navigation-menu').slideToggle(function(){
-			$('#js-mobile-menu').toggleClass('active');
-			if($('#js-navigation-menu').is(':hidden')) {
-				$('#js-navigation-menu').removeAttr('style');
-			}
+		menuToggle.on('click', function(e) {
+			e.preventDefault();
+			$('#js-navigation-menu').slideToggle(function() {
+				$('#js-mobile-menu').toggleClass('active');
+				if ($('#js-navigation-menu').is(':hidden')) {
+					$('#js-navigation-menu').removeAttr('style');
+				}
+			});
+		})
+
+		// Search Bar
+
+		$('.search-tools').removeClass("show");
+		$('#js-search-toggle').attr({
+			href: "javascript:void(0)"
 		});
-	})
-
-	// Search Bar
-
-	$('.search-tools').removeClass("show");
-	$('#js-search-toggle').attr({
-		href: "javascript:void(0)"	});
 		$('#js-search-toggle').addClass("show");
 		$('#js-search-toggle').click(function() {
 			$(".search-tools").toggle();
 			$('#js-search-toggle').toggleClass('active');
 		});
-	
-		// Google Form
-	
-		var googleForm = $('form').jqGoogleForms({"formKey": "1xnRdbuaw7-fpO_qdRCN0LPoHwVfJpR5C7EcoP5QM_3E"});
-		$( "#contact-form").submit(function(event) {
-			event.preventDefault();
-			googleForm.sendFormData({
-				"entry.993658079": $("input[name=name]").val(),
-				"entry.1139272614": $("input[name=email]").val(),
-				"entry.415731429": $("textarea[name=message]").val()
-			});
-			$( ".flash-success").fadeIn("fast");
-			$( "#contact-form").fadeOut("fast");
+
+		var googleForm = $('#contact-form').jqGoogleForms({
+			"formKey": "1xnRdbuaw7-fpO_qdRCN0LPoHwVfJpR5C7EcoP5QM_3E"
 		});
-	
-	});
 
-	(function (jQuery) {
-		jQuery.mark = {
-			jump: function (options) {
-				var defaults = {
-					selector: 'a.scroll-on-page-link'
-				};
-				if (typeof options == 'string') {
-					defaults.selector = options;
+		// Validate Forms
+
+		$("#contact-form").validate({
+				rules: {
+					email: "required",
+				},
+				messages: {
+					email: "We need your email to send you updates.",
+				},
+				submitHandler: function(form) {
+					googleForm.sendFormData({
+						"entry.993658079": $("input[name=name]").val(),
+						"entry.1139272614": $("input[name=email]").val(),
+						"entry.415731429": $("textarea[name=message]").val()
+					});
+					$(".flash-success").fadeIn("fast");
+					$("#contact-form").fadeOut("fast");
 				}
+		});
+});
 
-				options = jQuery.extend(defaults, options);
-				return jQuery(options.selector).click(function (e) {
-					var jumpobj = jQuery(this);
-					var target = jumpobj.attr('href');
-					var thespeed = 1000;
-					var offset = jQuery(target).offset().top;
-					jQuery('html,body').animate({
-						scrollTop: offset
-					}, thespeed, 'swing');
-					e.preventDefault();
-				});
+(function(jQuery) {
+	jQuery.mark = {
+		jump: function(options) {
+			var defaults = {
+				selector: 'a.scroll-on-page-link'
+			};
+			if (typeof options == 'string') {
+				defaults.selector = options;
 			}
-		};
-	})(jQuery);
+
+			options = jQuery.extend(defaults, options);
+			return jQuery(options.selector).click(function(e) {
+				var jumpobj = jQuery(this);
+				var target = jumpobj.attr('href');
+				var thespeed = 1000;
+				var offset = jQuery(target).offset().top;
+				jQuery('html,body').animate({
+					scrollTop: offset
+				}, thespeed, 'swing');
+				e.preventDefault();
+			});
+		}
+	};
+})(jQuery);
 
 
-	jQuery(function(){  
-		jQuery.mark.jump();
-	});
+jQuery(function() {
+	jQuery.mark.jump();
+});
